f2cae122b98629147f8d2e23a865d6924dbe51a4
ZhangJunTao <zhangjuntao@ZhangJunTaodeMacBook-Pro.local>
2016-09-04 11:04:59 +0800

    test Dict

--- db.py
+++ db.py
@@ -2,13 +2,144 @@
 #db.py
 #-*- coding:utf-8 -*-
 
+import time
+import uuid
+import functools
+import threading
+import logging
+
+engine = None
+
+class Dict(dict):
+	def __init__(self,names=(),values=(),**kw):
+		super(Dict,self).__init__(**kw)
+		for k,v in zip(names,values):
+			self[k] = v
+
+	def __getattr__(self,key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attribute '%s'" %key)
+	def __setattr__(self,key,value):
+		self[key] = value
+
+
+'''		
+def next_id(t=None):
+	if t is None:
+		t = time.time()
+	return '%015d%s000' %(int(t*1000),uuid.uuid4().hex)
+
+def _profiling(start,sql=''):
+	t = time.time() - start
+	if t > 0.1:
+		logging.warning('[PROFILING] [DB] %s: %s' %(t,sql))
+	else:
+        logging.info('[PROFILING] [DB] %s: %s' % (t, sql))
+
+def create_engine(user,password,database,host='127.0.0.1',port=3306,**kw):
+	import mysql.connector
+	global engine
+	if engine is not None:
+		raise DBError('Engine is already initialized.')
+	params= dict(user=user,password=password,database=database,host=host,port=port)
+	defaults =dict(use_unicode=True,charset='utf8',collation='utf8_general_ci',autocommit=False)
+	for k,v in defaults.iteritems():
+		params[k] = kw.pop(k,v)
+	params.update(kw)
+	params['buffered']= True
+	engine = _Engine(lambda: mysql.connector.connect(**params))
+	logging.info('Init mysql engine <%s> ok.' %hex(id(engine)))
+
+def connection():
+	return _ConnectionCtx()		
+
+def with_connection(func):
+	@functools.wraps()
+	def _wrapper(*args,**kw):
+		with _ConnectionCtx()
+			return func(*args,**kw)
+	return _wrapper
+
+class transaction():
+	return _TransactionCtx()
+
+def with_transaction(func):
+	@functools.wraps(func)
+	def _wrapper(*args,**kw):
+		start = time.time()
+		with _TransactionCtx()
+			func(*args,**kw)
+		_profiling(start)
+	retun _wrapper
+
+@with_connection
+def _select(sql,first,*args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?','%s')
+	logging.info('SQL: %s,ARGS: %s' %(sql,args))
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql,args)
+		if cursor.description:
+			names = [x[0] for x in cursor.description]
+		if first:
+			values = cursor.fetchone()
+			if not values:
+				return None
+			return Dict(names,values)
+		return [Dict(names,x) for x in cursor.fetchall()]
+	finally:
+		if cursor:
+			cursor.close()
+
+def select_one(sql,*args):
+	return _select(sql,True,*args)
+
+def select_int(sql,*args):
+	d = _select(sql,True,*args)
+	if len(d) != 1:
+		raise MultiColumnsError('Expect only one column.')
+	return d.values()[0]
+
+def select(sql,*args):
+	return _select(sql,False,*args)
+
+@with_connection
+def _update(sql,*args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?','%s')
+	logging.info('SQL: %s,ARGS:%s' %(sql,args))
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql,args)
+		r = cursor.rowcount
+		if _db_ctx.transactions == 0:
+			logging.info('auto commit')
+			_db.ctx.connection.commit()
+		return r
+	finally:
+		if cursor:
+			cursor.close()
+
+def update(sql,*args):
+	return _update(sql,*args)
+
+def insert(table,**kw):
+	cols, args = zip(*kw.iteritems())
+	sql = 'insert into `%s` (%s) values (%s)' %(table, ','.join(['?' for i in range(len(cols))]))
+	return _update(sql,*args)
+
 class _Engine(object):
 	def __init__(self,connect):
 		self._connect = connect
 	def connect(self):
 		return self._connect()
 
-engine = None
+
 
 class _Dbctx(threading.local):
 	def __init__(self):
@@ -45,8 +176,7 @@ class _ConnectionCtx(object):
 		if self.should_cleanup:
 			_db_ctx.cleanup()
 
-def connection():
-	return _ConnectionCtx()
+
 
 @with_connection
 def select(sql, *args):
@@ -89,3 +219,6 @@ class _TransactionCtx(object):
 	def rollback(self):
 		global _db_ctx
 		_db_ctx.connction.rollback()
+
+
+'''		
