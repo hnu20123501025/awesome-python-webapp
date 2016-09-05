69e3b0354b7fd31ac626b15b6c49d377958588a9
ZhangJunTao <zhangjuntao@ZhangJunTaodeMacBook-Pro.local>
2016-09-05 22:12:52 +0800

    db.py完全实现了

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
-#db.py
 #-*- coding:utf-8 -*-
+#db.py
+
 
 import time
 import uuid
@@ -11,7 +12,29 @@ import logging
 engine = None
 
 class Dict(dict):
+ 	'''
+	Simple dict but support access as x.y style.
+	>>> d1 = Dict()
+	>>> d1['x'] = 100
+	>>> d1.x
+	100
+	>>> d1.y = 200
+	>>> d1['y']
+	200
+	>>> d2 = Dict(a=1, b=2, c='3')
+	>>> d2.c
+	'3'
+	>>> d2['empty']
+	Traceback (most recent call last):
+	    ...
+	KeyError: 'empty'
+	>>> d2.empty
+	Traceback (most recent call last):
+	    ...
+	AttributeError: 'Dict' object has no attribute 'empty'
+    '''
 	def __init__(self,names=(),values=(),**kw):
+		# 这里的效果是继承dict.__init__ 
 		super(Dict,self).__init__(**kw)
 		for k,v in zip(names,values):
 			self[k] = v
@@ -25,7 +48,7 @@ class Dict(dict):
 		self[key] = value
 
 
-'''		
+
 def next_id(t=None):
 	if t is None:
 		t = time.time()
@@ -36,7 +59,68 @@ def _profiling(start,sql=''):
 	if t > 0.1:
 		logging.warning('[PROFILING] [DB] %s: %s' %(t,sql))
 	else:
-        logging.info('[PROFILING] [DB] %s: %s' % (t, sql))
+		logging.info('[PROFILING] [DB] %s: %s' % (t, sql))
+
+class DBError(Exception):
+	pass
+
+class MultiColumnsError(DBError):
+	pass
+
+class _LasyConnection(object):
+	def __init__(self):
+		self.connection = None
+
+	def cursor(self):
+		if self.connection is None:
+			connection = engine.connect()
+			logging.info('open connection <%s>...' %hex(id(connection)))
+			self.connection = connection
+		return self.connection.cursor()
+
+	def commit(self):
+		self.connection.commit()
+
+	def rollback(self):
+		self.connection.rollback()
+
+	def cleanup(self):
+		if self.connection:
+			connection = self.connection
+			self.connection = None
+			logging.info('close connection <%s>...' % hex(id(connection)))
+			connection.close()
+
+class _Dbctx(threading.local):
+	def __init__(self):
+		self.connection = None
+		self.transactions = 0
+	
+	def is_init(self):
+		return not self.connection is None
+
+	def init(self):
+		logging.info('open lazy connection')
+		self.connection = _LasyConnection()
+		self.transactions = 0
+
+	def cleanup(self):
+		self.connection.cleanup()
+		self.connection = None
+
+	def cursor(self):
+		return self.connection.cursor()
+
+_db_ctx = _Dbctx()
+
+engine = None
+
+class _Engine(object):
+	def __init__(self,connect):
+		self._connect = connect
+
+	def connect(self):
+		return self._connect()
 
 def create_engine(user,password,database,host='127.0.0.1',port=3306,**kw):
 	import mysql.connector
@@ -52,116 +136,6 @@ def create_engine(user,password,database,host='127.0.0.1',port=3306,**kw):
 	engine = _Engine(lambda: mysql.connector.connect(**params))
 	logging.info('Init mysql engine <%s> ok.' %hex(id(engine)))
 
-def connection():
-	return _ConnectionCtx()		
-
-def with_connection(func):
-	@functools.wraps()
-	def _wrapper(*args,**kw):
-		with _ConnectionCtx()
-			return func(*args,**kw)
-	return _wrapper
-
-class transaction():
-	return _TransactionCtx()
-
-def with_transaction(func):
-	@functools.wraps(func)
-	def _wrapper(*args,**kw):
-		start = time.time()
-		with _TransactionCtx()
-			func(*args,**kw)
-		_profiling(start)
-	retun _wrapper
-
-@with_connection
-def _select(sql,first,*args):
-	global _db_ctx
-	cursor = None
-	sql = sql.replace('?','%s')
-	logging.info('SQL: %s,ARGS: %s' %(sql,args))
-	try:
-		cursor = _db_ctx.connection.cursor()
-		cursor.execute(sql,args)
-		if cursor.description:
-			names = [x[0] for x in cursor.description]
-		if first:
-			values = cursor.fetchone()
-			if not values:
-				return None
-			return Dict(names,values)
-		return [Dict(names,x) for x in cursor.fetchall()]
-	finally:
-		if cursor:
-			cursor.close()
-
-def select_one(sql,*args):
-	return _select(sql,True,*args)
-
-def select_int(sql,*args):
-	d = _select(sql,True,*args)
-	if len(d) != 1:
-		raise MultiColumnsError('Expect only one column.')
-	return d.values()[0]
-
-def select(sql,*args):
-	return _select(sql,False,*args)
-
-@with_connection
-def _update(sql,*args):
-	global _db_ctx
-	cursor = None
-	sql = sql.replace('?','%s')
-	logging.info('SQL: %s,ARGS:%s' %(sql,args))
-	try:
-		cursor = _db_ctx.connection.cursor()
-		cursor.execute(sql,args)
-		r = cursor.rowcount
-		if _db_ctx.transactions == 0:
-			logging.info('auto commit')
-			_db.ctx.connection.commit()
-		return r
-	finally:
-		if cursor:
-			cursor.close()
-
-def update(sql,*args):
-	return _update(sql,*args)
-
-def insert(table,**kw):
-	cols, args = zip(*kw.iteritems())
-	sql = 'insert into `%s` (%s) values (%s)' %(table, ','.join(['?' for i in range(len(cols))]))
-	return _update(sql,*args)
-
-class _Engine(object):
-	def __init__(self,connect):
-		self._connect = connect
-	def connect(self):
-		return self._connect()
-
-
-
-class _Dbctx(threading.local):
-	def __init__(self):
-		self.connection = None
-		self.transactions = 0
-	
-	def is_init(self):
-		return not self.connection is None
-
-	def init(self):
-		self.connection = _LasyConnection()
-		self.transactions = 0
-
-	def cleanup(self):
-		self.connection.cleanup()
-		self.connection = None
-
-	def cursor(self):
-		return self.connection.cursor()
-
-_db_ctx = _Dbctx()
-
 class _ConnectionCtx(object):
 	def __enter__(self):
 		global _db_ctx
@@ -176,15 +150,15 @@ class _ConnectionCtx(object):
 		if self.should_cleanup:
 			_db_ctx.cleanup()
 
+def connection():
+	return _ConnectionCtx()		
 
-
-@with_connection
-def select(sql, *args):
-	pass
-
-@with_connection
-def update(sql,*args):
-	pass
+def with_connection(func):
+	@functools.wraps(func)
+	def _wrapper(*args,**kw):
+		with _ConnectionCtx():
+			return func(*args,**kw)
+	return _wrapper
 
 class _TransactionCtx(object):
 	def __enter__(self):
@@ -211,14 +185,103 @@ class _TransactionCtx(object):
 
 	def commit(self):
 		global _db_ctx
+		logging.info('commit transaction...')
 		try:
 			_db_ctx.connction.commit()
+			logging.info('commit ok.')
 		except:
+			logging.warning('commit failde. try rollback...')
 			_db_ctx.connction.rollback()
+			logging.warning('rollback ok.')
 			raise
+
 	def rollback(self):
 		global _db_ctx
+		logging.warning('rollback transaction...')
 		_db_ctx.connction.rollback()
+		logging.info('rollback ok.')
+
+def transaction():
+	return _TransactionCtx()
+
+def with_transaction(func):
+	@functools.wraps(func)
+	def _wrapper(*args,**kw):
+		_start = time.time()
+		with _TransactionCtx():
+			func(*args,**kw)
+		_profiling(_start)
+	return _wrapper
+
+@with_connection
+def _select(sql,first,*args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?','%s')
+	logging.info('SQL: %s,ARGS: %s' %(sql,args))
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql,args)
+		if cursor.description:
+			names = [x[0] for x in cursor.description]
+		if first:
+			values = cursor.fetchone()
+			if not values:
+				return None
+			return Dict(names,values)
+		return [Dict(names,x) for x in cursor.fetchall()]
+	finally:
+		if cursor:
+			cursor.close()
+
+@with_connection
+def select_one(sql,*args):
+	return _select(sql,True,*args)
+
+@with_connection
+def select_int(sql,*args):
+	d = _select(sql,True,*args)
+	if len(d) != 1:
+		raise MultiColumnsError('Expect only one column.')
+	return d.values()[0]
+
+@with_connection
+def select(sql,*args):
+	return _select(sql,False,*args)
+
+@with_connection
+def _update(sql,*args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?','%s')
+	logging.info('SQL: %s,ARGS:%s' %(sql,args))
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql,args)
+		r = cursor.rowcount
+		if _db_ctx.transactions == 0:
+			logging.info('auto commit')
+			_db_ctx.connection.commit()
+		return r
+	finally:
+		if cursor:
+			cursor.close()
 
 
-'''		
+def insert(table,**kw):
+	cols, args = zip(*kw.iteritems())
+	sql = 'insert into `%s` (%s) values (%s)' %(table, ','.join(['?' for i in range(len(cols))]))
+	return _update(sql,*args)
+
+def update(sql,*args):
+	return _update(sql,*args)
+
+if __name__=='__main__':
+	logging.basicConfig(level = logging.DEBUG)
+	create_engine('root','','test')
+	update('drop table if exists user')
+	update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
+
+
+
+
